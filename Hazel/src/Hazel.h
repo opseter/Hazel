90e6599657e609e289bdec7e10da420c651af036
opseter <opseter@outlook.com>
2020-09-07 10:43:23 +0800

    init

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//for use by hazel applications
+#include"Hazel/Application.h"
+#include"Hazel/Log.h"
+//-------------Entry Point-------------
+
+#include"Hazel/EntryPoint.h"
+
+
+
+//-------------------------------------
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hazel
+Game
